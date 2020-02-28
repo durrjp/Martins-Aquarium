7f39d5251ae6cec5ef0beb32869223d8b5f40db6
durrjp <durrjp1@gmail.com>
2020-02-27 18:53:19 -0600

    working event listener automation or all fish dialog boxes/buttons

--- scripts/Dialogs/dialogs.js
+++ scripts/Dialogs/dialogs.js
@@ -26,61 +26,6 @@ const initializeDetailButtonEvents = () => {
             }
         )
     }
-    // document.querySelector("#button--Marvin").addEventListener(
-    //     "click",
-    //     theClickEvent => {
-    //         const theDialog = document.querySelector("#details--Marvin")
-    //         theDialog.showModal()
-    //     }
-    // )
-    // document.querySelector("#button--Jimmy").addEventListener(
-    //     "click",
-    //     theClickEvent => {
-    //         const theDialog = document.querySelector("#details--Jimmy")
-    //         theDialog.showModal()
-    //     }
-    // )
-    // document.querySelector("#button--Flip").addEventListener(
-    //     "click",
-    //     theClickEvent => {
-    //         const theDialog = document.querySelector("#details--Flip")
-    //         theDialog.showModal()
-    //     }
-    // )
-    // document.querySelector("#button--Slithers").addEventListener(
-    //     "click",
-    //     theClickEvent => {
-    //         const theDialog = document.querySelector("#details--Slithers")
-    //         theDialog.showModal()
-    //     }
-    // )
-    // document.querySelector("#button--Chubbers").addEventListener(
-    //     "click",
-    //     theClickEvent => {
-    //         const theDialog = document.querySelector("#details--Chubbers")
-    //         theDialog.showModal()
-    //     }
-    // )
-    // document.querySelector("#button--Chad").addEventListener(
-    //     "click",
-    //     theClickEvent => {
-    //         const theDialog = document.querySelector("#details--Chad")
-    //         theDialog.showModal()
-    //     }
-    // )
-    // document.querySelector("#button--Susan").addEventListener(
-    //     "click",
-    //     theClickEvent => {
-    //         const theDialog = document.querySelector("#details--Susan")
-    //         theDialog.showModal()
-    //     }
-    // )
-    // document.querySelector("#button--Alfredo").addEventListener(
-    //     "click",
-    //     theClickEvent => {
-    //         const theDialog = document.querySelector("#details--Alfredo")
-    //         theDialog.showModal()
-    //     }
-    // )
+    
 }
 export default initializeDetailButtonEvents
